f6539a06b46bbe9c081f0f5fedea2845bb85fcc7
go2null <1t1is2@gmail.com>
2017-08-17 16:24:54 -0400

    ansi-color: new helper library

---
+++ ansi-colors.sh
@@ -0,0 +1,39 @@
+# colors
+# https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
+# http://www.bashguru.com/2010/01/shell-colors-colorizing-shell-scripts.html
+# http://www.pix:elbeat.org/docs/terminal_colours/
+
+# background-color
+export B_BLACK='\033[40m'
+export B_RED='\033[41m'
+export B_GREEN='\033[42m'
+export B_YELLOW='\033[43m'
+export B_BLUE='\033[44m'
+export B_MAGENTA='\033[45m'
+export B_CYAN='\033[46m'
+export B_WHITE='\033[47m'
+
+# (foreground) color
+export F_BLACK='\033[0;30m'
+export F_RED='\033[0;31m'
+export F_GREEN='\033[0;32m'
+export F_YELLOW='\033[0;33m'
+export F_BLUE='\033[0;34m'
+export F_MAGENTA='\033[0;35m'
+export F_CYAN='\033[0;36m'
+export F_GRAY='\033[0;37m'
+export F_DARK_GRAY='\033[1;30m'
+export F_BRIGHT_RED='\033[1;31m'
+export F_BRIGHT_GREEN='\033[1;32m'
+export F_BRIGHT_YELLOW='\033[1;33m'
+export F_BRIGHT_BLUE='\033[1;34m'
+export F_BRIGHT_MAGENTA='\033[1;35m'
+export F_BRIGHT_CYAN='\033[1;36m'
+export F_WHITE='\033[1;37m'
+
+# text-decoration
+export T_NORMAL='\033[0m'
+export T_BOLD='\033[1m'
+export T_UNDERLINE='\033[4m'
+export T_BLINK='\033[5m'
+export T_REVERSE='\033[7m'
